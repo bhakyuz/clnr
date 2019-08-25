d613ab5cc8c4375d288bfe5fe0d195394211d9ce
bahadir <bhakyuz@gmail.com>
2019-08-26 00:16:55 +0200

    Add logs

--- R/clean.R
+++ R/clean.R
@@ -5,48 +5,53 @@
 #' @return chacacter vector of clean/unified strings
 #' @importFrom magrittr "%>%"
 #' @export
-clean <- function(s, ask_user = T){
-  df <- data.frame(s = s, fingerprint = fingerprint(s), stringsAsFactors = F)
+clean <- function(s, ask_user = TRUE){
+  
+  df <- data.frame(s = s, 
+                   fingerprint = fingerprint(s), 
+                   stringsAsFactors = FALSE)
   
   df <- 
     df %>% 
-    dplyr::mutate(l = nchar(s)) %>%
+    dplyr::mutate(length = nchar(s)) %>%
     dplyr::group_by(s) %>%
     dplyr::mutate(count = dplyr::n()) %>%
     dplyr::group_by(fingerprint, s) %>%
     dplyr::mutate(count_combination = dplyr::n()) %>%
     dplyr::group_by(fingerprint) %>% 
-    # value_to_keep is based on the original data for now but this 
-    # ideally this might be provided by user 
-    # so it is more reproducible
     dplyr::mutate(value_to_keep = dplyr::last(s, order_by = count, )) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(will_be_updated = !value_to_keep == s) 
-  
-  # paste(df$s[!df$change], df$u[!df$change], sep = ">", collapse = " ")
   
   to_update <- 
     df %>%
     dplyr::filter(will_be_updated) %>%
     dplyr::mutate(
-      prio_to_update = (count + count_combination * .01 + l * .001) / nrow(df),
+      prio_to_update = (count + count_combination * .01 + length * .001) / nrow(df),
       final_value_to_keep = as.character(NA)
     ) %>%
-    dplyr::arrange(dplyr::desc(prio_to_update), dplyr::desc(count), dplyr::desc(count_combination), l) %>%
-    #   dplyr::select(s, fingerprint, value_to_keep) %>% 
+    dplyr::arrange(
+      dplyr::desc(prio_to_update), 
+      dplyr::desc(count), dplyr::desc(count_combination), length) %>%
     dplyr::distinct()
   
-  for (i in 1:nrow(to_update)) {
-    to_update$final_value_to_keep[i] <- get_feedback(
-      x = to_update$s[i], 
-      replacement = to_update$value_to_keep[i], ask_user)
+  if(!ask_user) {
+    to_update$final_value_to_keep <- to_update$value_to_keep
+  } else {
+    for (i in 1:nrow(to_update)) {
+      to_update$final_value_to_keep[i] <- get_feedback(
+        x = to_update$s[i], 
+        replacement = to_update$value_to_keep[i], ask_user)
+    }
   }
   
-  df2 <- dplyr::left_join(
+  df_final <- dplyr::left_join(
     df, 
     dplyr::select(to_update, s, final_value_to_keep),
     by = "s")
   
-  res <- dplyr::coalesce(df2$final_value_to_keep, df2$s)
+  logs <- to_update %>%
+    dplyr::select(s, fingerprint, final_value_to_keep)
+  res <- dplyr::coalesce(df_final$final_value_to_keep, df_final$s)
   return(res)
 }
